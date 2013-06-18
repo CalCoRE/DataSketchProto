9604600975779da72bd67ae20eacc2024fde5673
Sabrina Gordon <sabrina.r.gordon@gmail.com>
2013-06-18 18:47:52 -0400

    Beginning stages of two demos, added jquery ui css

---
+++ vectorEditor/demos.js
@@ -0,0 +1,26 @@
+function skinsDemo(){
+
+$("#skinstart").slideDown();
+$("#skinstart").accordion();
+$("#skinstart").css("display", "block");
+	//draw unopened flower as object
+	
+	//add 3 growing stages of flower as skins
+	
+	//use slider to see the flower open and close
+	
+	//drag objects and skins all together
+}
+
+function dataDemo(){
+
+$("#datastart").slideDown();
+$("#datastart").accordion();
+$("#datastart").css("display", "block");
+
+	//load in csv data
+	
+	//drag and drop labels on objects
+	
+	//play data
+}
